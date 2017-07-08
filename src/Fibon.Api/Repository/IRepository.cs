13bbe19263d18a69339a6b59ebc50b6fe8be43bb
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-07-08 11:11:48 +0200

    Added repository

---
+++ src/Fibon.Api/Repository/IRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Fibon.Api.Repository
+{
+    public interface IRepository
+    {
+        void Insert(int number, int value);
+        int? Get(int number);
+    }
+
+    public class RepositoryInMemory : IRepository
+    {
+        private readonly Dictionary<int, int> _storage = new Dictionary<int, int>();
+
+        public void Insert(int number, int value)
+        {
+            _storage[number] = value;
+        }
+
+        public int? Get(int number)
+        {
+            int value;
+            if (_storage.TryGetValue(number, out value))
+            {
+                return value;
+            }
+
+            return null;
+        }
+    }
+}
