73507fd51fb554206241f6b67aff56471703cba1
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-07-08 11:32:38 +0200

    Added business logic to the controller

--- src/Fibon.Api/Controllers/FibonacciController.cs
+++ src/Fibon.Api/Controllers/FibonacciController.cs
@@ -1,5 +1,8 @@
+using System.Threading.Tasks;
 using Fibon.Api.Repository;
+using Fibon.Messages.Commands;
 using Microsoft.AspNetCore.Mvc;
+using RawRabbit;
 
 namespace Fibon.Api.Controllers
 {
@@ -7,21 +10,35 @@ namespace Fibon.Api.Controllers
     public class FibonacciController : Controller
     {
         private readonly IRepository _repository;
+        private readonly IBusClient _busClient;
 
-        public FibonacciController(IRepository repository)
+        public FibonacciController(IRepository repository, IBusClient busClient)
         {
             _repository = repository;
+            _busClient = busClient;
         }
 
         [HttpGet("{number}")]
         public IActionResult Get(int number)
         {
-            return Content("0");
+            int? result = _repository.Get(number);
+            if (result.HasValue)
+            {
+                return Content(result.Value.ToString());
+            }
+
+            return Content("Not ready...");
         }
 
         [HttpPost("{number}")]
-        public IActionResult Post(int number)
+        public async Task<IActionResult> Post(int number)
         {
+            int? result = _repository.Get(number);
+            if (!result.HasValue)
+            {
+                await _busClient.PublishAsync(new CalculateValueCommand { Number = number });
+            }
+
             return Accepted($"fibonacci/{number}", null);
         }
     }
