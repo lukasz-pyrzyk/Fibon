750163912c25b2e09c4065bbcbb32c5d48e71189
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-07-08 11:41:45 +0200

    Added command handler

---
+++ src/Fibon.Service/Handlers/CalculateValueCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Fibon.Messages.Commands;
+using Fibon.Messages.Events;
+using RawRabbit;
+
+namespace Fibon.Service.Handlers
+{
+    public class CalculateValueCommandHandler : ICommandHandler<CalculateValueCommand>
+    {
+        private readonly IBusClient _busClient;
+
+        public CalculateValueCommandHandler(IBusClient busClient)
+        {
+            _busClient = busClient;
+        }
+
+        public async Task HandleAsync(CalculateValueCommand command)
+        {
+            int result = Fib(command.Number);
+
+            await _busClient.PublishAsync(new ValueCalculatedEvent { Number = command.Number, Value = result });
+        }
+
+        private int Fib(int n)
+        {
+            switch (n)
+            {
+                case 0:
+                    return 0;
+                case 1:
+                    return 1;
+                default:
+                    return Fib(n - 2) + Fib(n - 1);
+            }
+        }
+    }
+}
